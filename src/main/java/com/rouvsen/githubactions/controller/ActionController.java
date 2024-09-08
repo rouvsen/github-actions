30663ef86f4773aea4a001727c7f9d392c897988
rouvsen <rouvsen@gmail.com>
2024-09-08 15:01:23 +0400

    add testCaching api

--- src/main/java/com/rouvsen/githubactions/controller/ActionController.java
+++ src/main/java/com/rouvsen/githubactions/controller/ActionController.java
@@ -2,6 +2,7 @@ package com.rouvsen.githubactions.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class ActionController {
     @GetMapping
     public ResponseEntity<String> action() {
         return ResponseEntity.ok("Github Action Version: %s".formatted("0.1.0"));
+    }
+
+    @GetMapping("/test-caching")
+    public ResponseEntity<String> testCaching(@RequestHeader String header) {
+        return ResponseEntity.ok("Hi, %s".formatted(header));
     }
 }
